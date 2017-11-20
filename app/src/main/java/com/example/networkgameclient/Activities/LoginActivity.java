386d74d2282d4f5afa15565b0160be53fee085c1
이승헌 <zakartayo@naver.com>
2017-11-21 00:41:35 +0900

    네트워크 채팅 게임 두번째 커밋
    닉네임 로그인 자바 서버에 접속 메인 서버 채팅 구현

--- app/src/main/java/com/example/networkgameclient/Activities/LoginActivity.java
+++ app/src/main/java/com/example/networkgameclient/Activities/LoginActivity.java
@@ -1,13 +1,15 @@
-package com.example.networkgameclient;
+package com.example.networkgameclient.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.networkgameclient.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
